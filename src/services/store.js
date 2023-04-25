1d589b27617c60890d24ca2b1cd56ec8b4b91362
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-04-25 15:48:27 +0530

    nn

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import {articleApi} from './article'
+import { articleApi } from './article'
 
 export const store = configureStore({
     reducer: {
