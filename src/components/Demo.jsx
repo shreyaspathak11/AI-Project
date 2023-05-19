53d8db5531c921688879d494cb40fc98bc0111d3
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-19 20:04:42 +0530

    api started working

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,147 +1,60 @@
-import { useState, useEffect } from "react";
+import { useState } from 'react';
+import axios from 'axios';
 
-import { copy, linkIcon, loader, tick } from "../assets";
-import { useLazyGetSummaryQuery } from "../services/article";
+function SearchAndSummary() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [summary, getSummary] = useState('');
 
-const Demo = () => {
-  const [article, setArticle] = useState({
-    url: "",
-    summary: "",
-  });
-  const [allArticles, setAllArticles] = useState([]);
-  const [copied, setCopied] = useState("");
-
-  // RTK lazy query
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
-
-  // Load data from localStorage on mount
-  useEffect(() => {
-    const articlesFromLocalStorage = JSON.parse(
-      localStorage.getItem("articles")
-    );
-
-    if (articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage);
-    }
-  }, []);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const existingArticle = allArticles.find(
-      (item) => item.url === article.url
-    );
-
-    if (existingArticle) return setArticle(existingArticle);
-
-    const { data } = await getSummary({ articleUrl: article.url });
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...allArticles];
-
-      // update state and local storage
-      setArticle(newArticle);
-      setAllArticles(updatedAllArticles);
-      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
-    }
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
   };
 
-  // copy the url and toggle the icon for user feedback
-  const handleCopy = (copyUrl) => {
-    setCopied(copyUrl);
-    navigator.clipboard.writeText(copyUrl);
-    setTimeout(() => setCopied(false), 3000);
-  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  const handleKeyDown = (e) => {
-    if (e.keyCode === 13) {
-      handleSubmit(e);
+    const options = {
+      method: 'GET',
+      url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+      params: {
+        url: searchTerm,
+        length: '3'
+      },
+      headers: {
+        'X-RapidAPI-Key': '09b452bdf9msha2b0e2c364f30f8p1fd296jsn142bf2d30b09',
+        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      getSummary(JSON.stringify(response.data));
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return (
-    <section className='mt-16 w-full max-w-xl'>
-      {/* Search */}
-      <div className='flex flex-col w-full gap-2'>
-        <form
-          className='relative flex justify-center items-center'
-          onSubmit={handleSubmit}
-        >
-          <img
-            src={linkIcon}
-            alt='link-icon'
-            className='absolute left-0 my-2 ml-3 w-5'
-          />
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Enter article URL"
+        />
+        <button type="submit">Summarize</button>
+      </form>
 
-          <input
-            type='url'
-            placeholder='Paste the article link'
-            value={article.url}
-            onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            onKeyDown={handleKeyDown}
-            required
-            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
-          />
-          <button
-            type='submit'
-            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
-          >
-            <p>↵</p>
-          </button>
-        </form>
+     { console.log(summary.summary)}
 
-        {/* Browse History */ }
-        <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
-          {allArticles.reverse().map((item, index) => (
-            <div
-              key={`link-${index}`}
-              onClick={() => setArticle(item)}
-              className='link_card'
-            >
-              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
-                <img
-                  src={copied === item.url ? tick : copy}
-                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
-                  className='w-[40%] h-[40%] object-contain'
-                />
-              </div>
-              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
-                {item.url}
-              </p>
-            </div>
-          ))}
+      {summary && (
+        <div>
+          <h2>Article Summary</h2>
+          <p>{summary}</p>
         </div>
-      </div>
-
-      {/* Display Result */}
-      <div className='my-10 max-w-full flex justify-center items-center'>
-        {isFetching ? (
-          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
-        ) : error ? (
-          <p className='font-inter font-bold text-black text-center'>
-            Well, that wasnot supposed to happen...
-            <br />
-            <span className='font-satoshi font-normal text-gray-700'>
-              {error?.data?.error}
-            </span>
-          </p>
-        ) : (
-          article.summary && (
-            <div className='flex flex-col gap-3'>
-              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
-                Article <span className='blue_gradient'>Summary</span>
-              </h2>
-              <div className='summary_box'>
-                <p className='font-inter font-medium text-sm text-gray-700'>
-                  {article.summary}
-                </p>
-              </div>
-            </div>
-          )
-        )}
-      </div>
-    </section>
+      )}
+    </div>
   );
-};
+}
 
-export default Demo;
+export default SearchAndSummary;
