b980dc51d1e80f279093a7356d471a6490de633e
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-19 21:51:54 +0530

    done

---
+++ src/components/Main.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+import axios from 'axios';
+import linkIcon from '../assets/link.svg';
+import loader from '../assets/loader.svg';
+
+function SearchAndSummary() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsLoading(true);
+    setError('');
+
+    const options = {
+      method: 'GET',
+      url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+      params: {
+        url: searchTerm,
+        length: '3'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      setArticle({
+        url: response.data.url,
+        summary: JSON.stringify(response.data.summary)
+      });
+    } catch (error) {
+      setError('An error occurred. Please try again.');
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
+
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            value={searchTerm}
+            onChange={handleChange}
+            required
+            className='url_input peer'
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
+          >
+            <p>↵</p>
+          </button>
+        </form>
+      </div>
+
+      {/* Display Result */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isLoading ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that was not supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default SearchAndSummary;
