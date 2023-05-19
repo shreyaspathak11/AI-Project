862b2b2e86310c6bf0e2f7602cd06c2d13ced188
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-19 21:33:28 +0530

    done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 import axios from 'axios';
+import linkIcon from '../assets/link.svg';
+import loader from '../assets/loader.svg';
 
 function SearchAndSummary() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [summary, getSummary] = useState('');
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -11,6 +18,9 @@ function SearchAndSummary() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    setIsLoading(true);
+    setError('');
 
     const options = {
       method: 'GET',
@@ -27,33 +37,77 @@ function SearchAndSummary() {
 
     try {
       const response = await axios.request(options);
-      getSummary(JSON.stringify(response.data));
+      setArticle({
+        url: response.data.url,
+        summary: JSON.stringify(response.data.summary)
+      });
     } catch (error) {
+      setError('An error occurred. Please try again.');
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleChange}
-          placeholder="Enter article URL"
-        />
-        <button type="submit">Summarize</button>
-      </form>
+    <section className='mt-16 w-full max-w-xl'>
+      {/* Search */}
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt='link-icon'
+            className='absolute left-0 my-2 ml-3 w-5'
+          />
 
-     { console.log(summary.summary)}
+          <input
+            type='url'
+            placeholder='Paste the article link'
+            value={searchTerm}
+            onChange={handleChange}
+            required
+            className='url_input peer'
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'
+          >
+            <p>↵</p>
+          </button>
+        </form>
+      </div>
 
-      {summary && (
-        <div>
-          <h2>Article Summary</h2>
-          <p>{summary}</p>
-        </div>
-      )}
-    </div>
+      {/* Display Result */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isLoading ? (
+          <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
+        ) : error ? (
+          <p className='font-inter font-bold text-black text-center'>
+            Well, that was not supposed to happen...
+            <br />
+            <span className='font-satoshi font-normal text-gray-700'>
+              {error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
   );
 }
 
